74d13bba0181732868b6dba836fc10c3e71cc421
Johan Kanefur <johan.canefur@gmail.com>
2016-12-12 13:30:18 +0100

    Updated way of getting container name, to support extending docker-compose files

--- scripts/containername.sh
+++ scripts/containername.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 # Figure out the main container name
-DIR=${PWD##*/}
 MAIN_SERVICE_NAME=$(cat docker-compose.yml | \
   grep ": # Main" | \
   grep -o -E '\s{2}(\w+):' | \
   grep -o -E '(\w+)')
 
-echo "${DIR}_${MAIN_SERVICE_NAME}_1"
+docker ps -f name="${MAIN_SERVICE_NAME}" -q
