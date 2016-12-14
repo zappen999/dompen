0d874c80936366e3209716e2ba40ce5ac9e57bf6
Johan Kanefur <johan.canefur@gmail.com>
2016-12-14 16:21:13 +0100

    Cross-platform support when opening coverage report, added missing mount of coverage dir

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export TERM=xterm
-rm -r ./coverage
+rm -r ./coverage/*
 ./node_modules/.bin/nyc --show-process-tree \
   ./node_modules/.bin/mocha --harmony-async-await './src/**/*.spec.js'
