0126c6012205188b0932f1e9e8127d79573921a6
Johan Kanefur <johan.canefur@gmail.com>
2017-02-15 14:39:53 +0100

    Forwarded SIGTERM signal to node script

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
+# SIGTERM handler
+handler() {
+  if [ $pid -ne 0]; then # 0 is the docker entrypoint
+    # tell the node process to gracefully shut down
+    kill -SIGTERM "$pid"
+
+    # wait for it to die
+    wait "$pid"
+  fi
+  exit 143 # 128 + 15 = SIGTERM
+}
+
+# setup callback handler
+trap 'kill ${!}; handler' SIGTERM
+
 if [ "$NODE_ENV" = "production" ]; then
   echo "Running in production mode..."
-  node --harmony-async-await src/index.js
+  node --harmony-async-await src/index.js &
 elif [ "$NODE_ENV" = "development" ]; then
   echo "Running in development mode. Installing dev-dependencies..."
   yarn
-  ./node_modules/.bin/nodemon --inspect=9222 --harmony-async-await src/index.js
+  ./node_modules/.bin/nodemon --inspect=9222 --harmony-async-await src/index.js &
 fi
+
+pid="$!" # get the PID from the last output (started process)
+echo "Process $pid spawned"
+
+# wait forever
+while true
+do
+  tail -f /dev/null & wait ${!}
+done
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 'use strict';
-
 console.log('Microservice started');
+
+process.on('SIGTERM', function() {
+  console.log('Shutting down gracefully...');
+  process.exit(0);
+});
 
 module.exports = {
   something: 'something'
